c1e218c664127abfe21289437920f25d7e1c0ace
Jemima MacKenzie <jemimamackenzie@gmail.com>
2024-04-28 15:58:32 +0000

    feat: Add secret key

--- gabbygourmand/settings.py
+++ gabbygourmand/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3wqr!tr+&s*h8c#c1^k+&ne(wb@2pn91izqs3ak!3vw96sg+3o'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
