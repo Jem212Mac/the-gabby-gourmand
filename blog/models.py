8ef42346f6bffc191686c9043d5c39feaf268de7
Jemima MacKenzie <jemimamackenzie@gmail.com>
2024-04-30 14:55:33 +0000

    fix:Revert back to using db.sqlite3 to check if error caused by migration issue

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 STATUS = ((0, "Draft"), (1, "Published"))
 RATING = ((5, "Excellent - 5 Stars"), (4, "Good - 4 Stars"), (3, "Ok - 3 Stars"), (2, "Could Be Better - 2 Stars"), (1, "Poor - 1 Star"), (0, "Really Bad - 0 Stars"))
-COST = ((0, "Low"), (1, "Medium"), (2, "High"))
+PRICE = ((0, "Low"), (1, "Medium"), (2, "High"))
 
 # Create your models here.
 class Review(models.Model):
@@ -12,12 +12,12 @@ class Review(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_reviews"
     )
-    restaurant_name = models.CharField(max_length=200)
+    restaurant = models.CharField(max_length=200)
     content = models.TextField()
     location = models.CharField(max_length=200)
     visited_on = models.DateTimeField()
     rating = models.IntegerField(choices=RATING, default=5)
-    cost = models.IntegerField(choices=COST, default=0)
+    price = models.IntegerField(choices=PRICE, default=0)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     updated_on = models.DateTimeField(auto_now=True)
