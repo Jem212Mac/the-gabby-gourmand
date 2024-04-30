2bf5cee509534cbf4a459cfbb3505047d856c1e4
Jemima MacKenzie <jemimamackenzie@gmail.com>
2024-04-30 15:36:42 +0000

    feat: Add comment model

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Review, Recipe
+from .models import Review, Recipe, Comment
 
 admin.site.register(Review)
 admin.site.register(Recipe)
+admin.site.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -35,3 +35,13 @@ class Recipe(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     updated_on = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
