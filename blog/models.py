e413848d804f4f515167bc05a98e147dc36e035a
Jemima MacKenzie <jemimamackenzie@gmail.com>
2024-04-30 15:22:46 +0000

    feat: Add recipe model

--- blog/models.py
+++ blog/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 STATUS = ((0, "Draft"), (1, "Published"))
 RATING = ((5, "Excellent - 5 Stars"), (4, "Good - 4 Stars"), (3, "Ok - 3 Stars"), (2, "Could Be Better - 2 Stars"), (1, "Poor - 1 Star"), (0, "Really Bad - 0 Stars"))
 PRICE = ((0, "Low"), (1, "Medium"), (2, "High"))
+TYPE = ((0, "Food"), (1, "Cocktail"))
 
-# Create your models here.
 class Review(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -18,6 +18,20 @@ class Review(models.Model):
     visited_on = models.DateTimeField()
     rating = models.IntegerField(choices=RATING, default=5)
     price = models.IntegerField(choices=PRICE, default=0)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    updated_on = models.DateTimeField(auto_now=True)
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_recipes"
+    )
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    type = models.IntegerField(choices=TYPE, default=0)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     updated_on = models.DateTimeField(auto_now=True)
