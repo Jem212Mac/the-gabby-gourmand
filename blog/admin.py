ad2859b14deebbb942c93f6437fbbacec9fb2071
Jemima MacKenzie <jemimamackenzie@gmail.com>
2024-05-01 10:53:16 +0000

    feat: add additional filter and search options to django admin for reviews and recipes

--- blog/admin.py
+++ blog/admin.py
@@ -4,17 +4,17 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Review)
 class ReviewAdmin(SummernoteModelAdmin):
-    list_display = ('title', 'slug', 'status')
-    search_fields = ['title']
-    list_filter = ('status',)
+    list_display = ('title', 'slug', 'author', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'author', 'created_on',)
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 
 @admin.register(Recipe)
 class RecipeAdmin(SummernoteModelAdmin):
-    list_display = ('title', 'slug', 'status')
-    search_fields = ['title']
-    list_filter = ('status',)
+    list_display = ('title', 'slug', 'type', 'author', 'status', 'created_on')
+    search_fields = ['title', 'ingredients', 'instructions']
+    list_filter = ('status', 'author', 'type', 'created_on',)
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('ingredients', 'instructions')
 
