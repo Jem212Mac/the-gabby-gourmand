85968610a368be58e5a765f9001c7c2b54a94650
Jemima MacKenzie <jemimamackenzie@gmail.com>
2024-05-01 09:35:43 +0000

    feat: Change views.py to all display published reviews

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,5 @@ from .models import Review
 
 # Create your views here.
 class ReviewList(generic.ListView):
-    queryset = Review.objects.all()
+    queryset = Review.objects.filter(status=1)
     template_name = "review_list.html"
