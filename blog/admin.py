37444bbe513fadc65463830e5e6a1c58aeaba4f0
Jemima MacKenzie <jemimamackenzie@gmail.com>
2024-05-01 10:19:19 +0000

    feat: Add django summernote for use with reviews and recipes

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from .models import Review, Recipe, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Review)
-admin.site.register(Recipe)
+@admin.register(Review)
+class ReviewAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
+@admin.register(Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('ingredients', 'instructions')
+
 admin.site.register(Comment)
