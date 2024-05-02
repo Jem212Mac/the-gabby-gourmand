4f68624001c2e10f8a21853caca1d0fc84aedaba
Jemima MacKenzie <jemimamackenzie@gmail.com>
2024-05-02 17:04:10 +0000

    feat: Add html files for food recipes and cocktail recipes

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ReviewList.as_view(), name='home'),
+    path('', views.FoodList.as_view(), name='food'),
+    path('', views.CocktailList.as_view(), name='cocktail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Review
+from .models import Review, Recipe, Comment
 
 
 # Create your views here.
 class ReviewList(generic.ListView):
     queryset = Review.objects.filter(status=1)
     template_name = "blog/index.html"
+    paginate_by = 6
+
+class FoodList(generic.ListView):
+    queryset = Recipe.objects.filter(status=1, type=0)
+    template_name = "blog/food.html"
+    paginate_by = 6
+
+class CocktailList(generic.ListView):
+    queryset = Recipe.objects.filter(status=1, type=1)
+    template_name = "blog/cocktail.html"
     paginate_by = 6
