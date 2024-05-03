0009c4b48f665b075b11e02244e3968de43426f7
Jemima MacKenzie <jemimamackenzie@gmail.com>
2024-05-03 10:07:11 +0000

    fix: Change to url paths and html templates to fix broken links to food recipes and cocktail recipes

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ReviewList.as_view(), name='home'),
-    path('', views.FoodList.as_view(), name='food'),
-    path('', views.CocktailList.as_view(), name='cocktail'),
+    path('food_recipes', views.FoodList.as_view(), name='food'),
+    path('cocktail_recipes', views.CocktailList.as_view(), name='cocktail'),
 ]
